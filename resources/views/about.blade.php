f326e2bd29d3a255544e83d3e4a011fba827cc8a
Sahan <sudeerasahan@gmail.com>
2018-10-09 15:19:48 +0530

    Init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Shopping Cart</title>
+  </head>
+  <body>
+    <h1>About Page 1</h1>
+  </body>
+</html>
